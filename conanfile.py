1a585c51d63e37b54b99e017ffb7a288479ed790
Igor Mironchik <igor.mironchik@gmail.com>
2023-01-29 12:21:33 +0300

    Try without libtiff.

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class DependenciesRecipe(ConanFile):
         self.requires("libjpeg/9e")
         self.requires("libpng/1.6.39")
         self.requires("zlib/1.2.13")
-        self.requires("libtiff/4.4.0")
+        #self.requires("libtiff/4.4.0")
         self.requires("libxml2/2.10.3")
         self.requires("libiconv/1.17")
         if self.settings.os == "Windows":
@@ -22,11 +22,11 @@ class DependenciesRecipe(ConanFile):
         self.options["libjpeg"].shared = True
         self.options["libpng"].shared = True
         self.options["zlib"].shared = True
-        self.options["libtiff"].shared = True
+        #self.options["libtiff"].shared = True
         self.options["libxml2"].shared = True
         self.options["libiconv"].shared = True
         self.options["fontconfig"].shared = True
 
     def imports(self):
         self.copy("*.dll", "bin", "bin")
-        #self.copy("*.dylib", "lib", "lib")
+        self.copy("*.dylib", "lib", "lib")
