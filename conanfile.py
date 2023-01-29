3c9745ada70b628cd61d5510e2212580b5201b25
Francesco Pretto <ceztko@gmail.com>
2023-01-29 01:04:32 +0100

    Restored libjpeg/9e

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class CompressorRecipe(ConanFile):
     def requirements(self):
         self.requires("openssl/1.1.1s")
         self.requires("freetype/2.12.1")
-        self.requires("libjpeg-turbo/2.1.4")
+        self.requires("libjpeg/9e")
         self.requires("libpng/1.6.39")
         self.requires("zlib/1.2.13")
         self.requires("libtiff/4.4.0")
@@ -19,7 +19,7 @@ class CompressorRecipe(ConanFile):
     def configure(self):
         self.options["openssl"].shared = True
         self.options["freetype"].shared = True
-        self.options["libjpeg-turbo"].shared = True
+        self.options["libjpeg"].shared = True
         self.options["libpng"].shared = True
         self.options["zlib"].shared = True
         self.options["libtiff"].shared = True
